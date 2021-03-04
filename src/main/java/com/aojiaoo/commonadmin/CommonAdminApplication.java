b6e1e3b8fc3cd54da54d95e13c022a802c819144
puremdq <puremdq@gmail.com>
2021-03-04 23:28:48 +0800

    初始化项目

---
+++ src/main/java/com/aojiaoo/commonadmin/CommonAdminApplication.java
@@ -0,0 +1,13 @@
+package com.aojiaoo.commonadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aojiaoo/commonadmin/CommonAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aojiaoo.commonadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
